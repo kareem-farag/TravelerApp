27b3f1120bf8065d7f6e818a3a84dbc1e4e227d4
Kareem Farag <kareemfarag@hotmail.com>
2020-04-17 03:33:14 +0200

    commit #1 : change the latitude & longitude to Egyptian Grand Museum location

--- app/src/main/java/com/example/travelerapp/MapsActivity.java
+++ app/src/main/java/com/example/travelerapp/MapsActivity.java
@@ -7,6 +7,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -39,8 +40,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng grandMuseum = new LatLng(29.994366, 31.118801);
+        mMap.addMarker(new MarkerOptions().position(grandMuseum).title("Marker in Egypt").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(grandMuseum,15));
+
     }
 }
